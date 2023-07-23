6b1f65ce2cffd46ac87de329f308fdeaf95acae5
abdullah-aladham <abdullah.aladham@gmail.com>
2023-07-23 18:32:39 +0300

    done project and it has some bugs to fix

---
+++ src/com/abdullah_aladham/project1/project1.java
@@ -0,0 +1,21 @@
+package com.abdullah_aladham.project1;
+
+public class project1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+	/*	int loop=0;
+			while(loop<10) {
+				System.out.println(loop+" time(s)");
+				if(loop==10) {
+					break;
+				}
+				loop++;
+			}*/
+		for(int i=0;i<1000000001;i+=1000) {
+	
+		}
+
+	}
+
+}
